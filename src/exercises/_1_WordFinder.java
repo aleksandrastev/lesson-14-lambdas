e9c2f7596497532efb913192c9f4be14a823d24c
aleksandrastev <leafsoul91@gmail.com>
2019-09-08 14:28:33 +0200

    WordFinder solved

--- src/exercises/_1_WordFinder.java
+++ src/exercises/_1_WordFinder.java
@@ -6,14 +6,19 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class _1_WordFinder {
 
 	public static void main(String[] args) {
 		/* Print all the words beginning with P that are over 10 characters. */
-		
+		Stream<String> streamOfWords = loadWordList().stream();
 
-		
+		List<String> selectedWords = streamOfWords.filter(word -> word.length() > 10)
+				.filter(word -> word.charAt(0) == 'P').collect(Collectors.toList());
+		System.out.println(selectedWords);
+
 	}
 
 	public static List<String> loadWordList() {
